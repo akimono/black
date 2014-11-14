ae9bbc7fefe1e63cacd04c947ef402b376349a26
akimono <frank.cioppettini@gmail.com>
2014-11-14 11:51:39 -0500

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Black::Application.config.secret_token = 'd1ea9cf373dafc1df61114df65f48c8950d1b5a55e91f883959f43e06c1648347c7d6d3e49875d3e7425f6e201e0d1bf24e4aded132cbda11abb6f5768e71d70'
